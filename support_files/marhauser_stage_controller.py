d466f2d16041e73979f32f29ded0a0024d01a8df
Eric Muckley <ericmuckley@gmail.com>
2020-02-26 12:43:01 -0500

    Update marhauser_stage_controller.py

--- support_files/marhauser_stage_controller.py
+++ support_files/marhauser_stage_controller.py
@@ -4,13 +4,12 @@ Created on Mon Feb  3 15:39:10 2020
 
 @author: a6q
 """
-import time
+
 import serial
 import numpy as np
 from serial.tools import list_ports
 
 import io
-
 
 def print_ports():
     """Print a list of avilable serial ports."""
@@ -18,37 +17,35 @@ def print_ports():
     print('Available serial ports:')
     [print(p.device) for p in ports]
 
-
 print_ports()
 
-address = 'COM17'
+address = 'COM5'
+
 dev = serial.Serial(port=address,
                     baudrate=9600,
                     timeout=2,
-                    write_timeout=0.5,
+                    write_timeout=1,
                     bytesize=serial.EIGHTBITS,
                     parity=serial.PARITY_NONE,
-                    stopbits=serial.STOPBITS_ONE)
+                    stopbits=serial.STOPBITS_TWO)
 
-
+'''
 sio = io.TextIOWrapper(io.BufferedRWPair(dev, dev))
-sio.write(str(r'?ver\r'))
+sio.write('?ver\r\r')
 sio.flush()
 message = sio.readline()
 print(message)
 
+'''
+dev.write(('?ver\r\r').encode())
 
-dev.write((r'?ver\r').encode())
+#dev.flushOutput()
+#dev.flushInput()
+#dev.write((r'?ver\r').encode())
+print(dev.readline().decode())
 
-dev.flushOutput()
-dev.flushInput()
-dev.write((r'?ver\r').encode())
-print(dev.readline())
 
-#dev.write(('?pos\r').encode())
-#print(dev.read(1))
-#print('-------------------')
+#print(dev.isOpen())
 
-print(dev.isOpen())
 dev.close()
 
